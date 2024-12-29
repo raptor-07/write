96e072ac95312e73387c3b3aaea782061db12b85
Hurmaan Ahmed <contactme.hurmaan@gmail.com>
2024-12-29 19:36:12 +0530

    feat: attach id in html

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,25 @@
-import { remark } from "remark";
+import remarkParse from "remark-parse";
 import html from "remark-html";
+import toc from "remark-toc";
+import { unified } from "unified";
+import remarkRehype from "remark-rehype";
+import rehypeSlug from "rehype-slug";
+import rehypeStringify from "rehype-stringify";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await unified()
+    .use(remarkParse)
+    .use(toc, {
+      heading: "Table of Contents",
+      tight: true,
+      maxDepth: 3,
+      ordered: false,
+      skip: "Author's",
+    })
+    .use(remarkRehype)
+    .use(rehypeSlug)
+    .use(rehypeStringify)
+    .process(markdown);
+
   return result.toString();
 }
